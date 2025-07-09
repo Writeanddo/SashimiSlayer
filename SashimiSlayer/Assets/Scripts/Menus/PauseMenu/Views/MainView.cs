fe96e416f5db0289a789c396bf2481987d0bf0e1
Arthur G <arthurgan29@gmail.com>
2025-07-09 02:07:59 -0700

    Added restart button, renamed beatmap load event

--- SashimiSlayer/Assets/Scripts/Menus/PauseMenu/Views/MainView.cs
+++ SashimiSlayer/Assets/Scripts/Menus/PauseMenu/Views/MainView.cs
@@ -1,6 +1,61 @@
+using Beatmapping;
+using Core.Scene;
+using Events.Core;
+using UnityEngine;
+using Button = UnityEngine.UI.Button;
+
 namespace Menus.PauseMenu.Views
 {
     public class MainView : PauseMenuView
     {
+        [Header("Events (In)")]
+
+        [SerializeField]
+        private BeatmapEvent _beatmapLoadedEvent;
+
+        [SerializeField]
+        private BeatmapEvent _beatmapUnloadedEvent;
+
+        [Header("Depends")]
+
+        [SerializeField]
+        private LevelLoaderMono _levelLoader;
+
+        [SerializeField]
+        private Button _restartButton;
+
+        private BeatmapConfigSo _currentBeatmap;
+
+        private void OnDestroy()
+        {
+            _beatmapLoadedEvent.RemoveListener(OnBeatmapLoaded);
+            _beatmapUnloadedEvent.RemoveListener(OnBeatmapUnloaded);
+            _restartButton.onClick.RemoveListener(RestartLevel);
+        }
+
+        public override void ViewAwake()
+        {
+            _beatmapLoadedEvent.AddListener(OnBeatmapLoaded);
+            _beatmapUnloadedEvent.AddListener(OnBeatmapUnloaded);
+            _restartButton.onClick.AddListener(RestartLevel);
+            _restartButton.interactable = false;
+        }
+
+        private void OnBeatmapLoaded(BeatmapConfigSo beatmap)
+        {
+            _currentBeatmap = beatmap;
+            _restartButton.interactable = true;
+        }
+
+        private void OnBeatmapUnloaded()
+        {
+            _currentBeatmap = null;
+            _restartButton.interactable = false;
+        }
+
+        private void RestartLevel()
+        {
+            _levelLoader.LoadLastBeatmapLevel();
+        }
     }
 }
